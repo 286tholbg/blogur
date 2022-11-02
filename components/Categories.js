d908fdbbe29a2e084325fc40dce5055659f02fe4
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-11-02 12:44:54 +1100

    added PostCard, PostWidgets, Categories components, and Header

---
+++ components/Categories.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Categories = () => {
+
+    return (
+        <div className="">
+            Categories
+        </div>
+    )
+}
+
+export default Categories
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
 
 const posts = [
   {title: "React Testing", excerpt: "Learn React Testing"},
@@ -7,7 +8,7 @@ const posts = [
 
 export default function Home () {
   return (
-    <div className='container mx-auto px-10 mb-8 bg-gray-300'>
+    <div className='container mx-auto px-10 mb-8 bg-gray-100'>
       <Head>
         <title>Blogur</title>
         <link rel="icon" href="/favicon.ico" />
@@ -15,16 +16,14 @@ export default function Home () {
 
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-            <div key={index}>
-              {post.title}
-              {post.excerpt}
-            </div>
-          ))}
+          {posts.map(post => <PostCard key={post.title} post={post}/>)}
         </div>
-      </div>
-      <div className='lg:col-span-4 col-span-1'>
-          <div className="lg:sticky relative top-8"></div>
+        <div className='lg:col-span-4 col-span-1'>
+            <div className="lg:sticky relative top-8">
+              <PostWidget />
+              <Categories />
+            </div>
+        </div>
       </div>
     </div>
   )

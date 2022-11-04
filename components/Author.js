21bececf0862cc9a53882dcb6727bee73a3723a5
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-11-04 12:59:01 +1100

    author page and details completed

--- components/Author.js
+++ components/Author.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
 
-const Author = () => {
+const Author = ({author}) => {
+
   return (
-    <div>
-       <h1>Author</h1>
+    <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20'>
+      <div className='absolute left-0 right-2 -top-14'>
+        <Image
+            alt={author.name}
+            unoptimized
+            height={100}
+            width={100}
+            className='align-middle rounded-full'
+            src={author.photo.url}
+        />
+      </div>
+        <h3 className='text-white mt-4 my-4 text-xl font-bold'>{author.name}</h3>
+        <p className='text-white text-lg'>{author.bio}</p>
     </div>
   )
 }
